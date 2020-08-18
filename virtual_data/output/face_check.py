2c33d769b18e2e5f381edbaae7db6a0aa6a0fb00
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-08-18 11:20:40 +0900

    changed graph so that plot factor vs facial values

--- virtual_data/output/face_check.py
+++ virtual_data/output/face_check.py
@@ -2,6 +2,11 @@ import matplotlib
 import numpy as np
 from func_new import *
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+N_RANGE = 60
+TARGET_FACTOR = 0
+TARGET_FACE = 0
 
 EMO_NUM = 4
 SIT_NUM = 10
@@ -19,9 +24,9 @@ j=0
 count_array=np.zeros((USR_NUM,4))
 graph_label = ["0.1<r<0.2","0.2<r<0.3","0.3<r<0.4","0.4<r"]
 
-def out_kitekan_correct(data_num, set_num):
-  factor = np.loadtxt("../factor_"+str(data_num)+".csv",delimiter=",")
-  kibun = np.loadtxt("../mental_"+str(data_num)+".csv",delimiter=",")
+def out_kitekan_correct(data_num):
+  factor = np.loadtxt("../factor_"+str(data_num+1)+".csv",delimiter=",")
+  kibun = np.loadtxt("../mental_"+str(data_num+1)+".csv",delimiter=",")
   if kibun.ndim == 0:
     length = 1
     kibun = np.array([kibun])
@@ -38,41 +43,53 @@ def out_kitekan_correct(data_num, set_num):
 
   return phi_out
 
-set_num = 29
-len_num =30
-
-for test_num in range(100):
-  phi_signal = np.zeros((EMO_NUM,test_num+1))
+for test_num in range(1,N_RANGE):
+  phi_signal = np.zeros((EMO_NUM,N_RANGE))
+  #phi_signal = np.zeros((EMO_NUM,test_num+1))
   weight = np.zeros((EMO_NUM,SIT_NUM))
-  #phi_out = np.zeros((set_num,SIT_NUM,EMO_NUM))
   corr_out = np.zeros((SIT_NUM,EMO_NUM))
-  phi_out = out_kitekan_correct(test_num,len_num)
-  signal = np.loadtxt("../face_"+str(test_num)+".csv",delimiter=",")
+  phi_out = out_kitekan_correct(N_RANGE-1)
+  #phi_out = out_kitekan_correct(test_num+1)
+  signal = np.loadtxt("../face_"+str(N_RANGE)+".csv",delimiter=",")
+  #signal = np.loadtxt("../face_"+str(test_num+1)+".csv",delimiter=",")
+  factor = np.loadtxt("../factor_"+str(N_RANGE)+".csv",delimiter=",")
   if test_num == 0:
     signal = np.array([signal])
+    factor = np.array([factor])
 
-  weight[0,:]= np.loadtxt("./hap_weight_test.csv")
-  weight[1,:]= np.loadtxt("./sup_weight_test.csv")
-  weight[2,:]= np.loadtxt("./ang_weight_test.csv")
-  weight[3,:]= np.loadtxt("./sad_weight_test.csv")
+  weight[0,:]= np.loadtxt("./hap_weight_"+str(test_num+1)+".csv")
+  weight[1,:]= np.loadtxt("./sup_weight_"+str(test_num+1)+".csv")
+  weight[2,:]= np.loadtxt("./ang_weight_"+str(test_num+1)+".csv")
+  weight[3,:]= np.loadtxt("./sad_weight_"+str(test_num+1)+".csv")
 
-  #weight[0,:]= np.loadtxt("./hap_weight_"+str(tag_number)+".csv")
-  #weight[1,:]= np.loadtxt("./sup_weight_"+str(tag_number)+".csv")
-  #weight[2,:]= np.loadtxt("./ang_weight_"+str(tag_number)+".csv")
-  #weight[3,:]= np.loadtxt("./sad_weight_"+str(tag_number)+".csv")
-
-  for t in range(test_num):
+  #for t in range(test_num+1):
+  for t in range(N_RANGE):
     for emo_num in range(EMO_NUM):
       #array = signal - phi_out[:,sit_num,:]
       for sit_num in range(SIT_NUM):
 
         phi_signal[emo_num,t] += weight[emo_num,sit_num]*phi_out[t,sit_num,emo_num]
 
-  plt.plot(phi_signal[0],label="phi out")
-  plt.plot(signal[:,0],label="signal")
-  #plt.ylim(0,1)
+  plt.scatter(factor[:,TARGET_FACTOR],phi_signal[TARGET_FACE],label="phi out"+str(test_num),color=cm.hsv(test_num/float(N_RANGE+20.0)),linewidth=0.5,linestyle='dashed')
+  #plt.plot(phi_signal[0]-signal[:,0],label="phi out",color=cm.hsv(test_num/60.0))
+  #x = np.linspace(0,45,45)
+  #if test_num%3==0:
+  #  plt.bar(x+test_num*0.1,phi_signal[0]-signal[:,0],label="phi out",color=cm.hsv(test_num/60.0),width=.1,alpha=0.5)
+
+  plt.ylim(-0.1,0.3)
   plt.legend()
-  plt.show()
+  """
+  if test_num is not 44:
+    plt.pause(.1)
+    plt.clf()
+  else:
+    plt.show()
+  """
+#standard
+plt.xlabel("val of facor["+str(TARGET_FACTOR)+"]")
+plt.ylabel("val of face["+str(TARGET_FACE)+"]")
+plt.plot(factor[:,TARGET_FACTOR],signal[:,TARGET_FACE],label="signal",linestyle='dashed')
+plt.show()
 
   #for emo_num in range(EMO_NUM):
   #np.savetxt('/home/kazumi/prog/quiz_check2016/jrm_test/'+str(i_name+1)+'/face_diff_'+xlabel[emo_num]+'-29.csv',face_diff[:,emo_num],delimiter=",")
